ba7b89d44463e666392ac2dc77257a816d106f11
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-27 11:17:20 -0400

    Adjusted search bar animation

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.querySelector("svg");setTimeout((()=>{e.style.backgroundColor="white"}),4e3)})();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const initialSetup = () => {
+    const svg = document.querySelector('svg');
+    setTimeout(() => {
+        svg.style.backgroundColor = 'white';
+    }, 4000);
+}
+
+initialSetup();
